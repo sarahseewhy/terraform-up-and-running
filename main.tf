1537caf79712f3e0732f63c6e61be5e924226b0e
Sarah C Y <sarahseewhy@gmail.com>
2024-01-30 10:20:44 +0000

    chore(init): initialise git directory

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_instance" "this" {
+  ami = "ami-09d6bbc1af02c2ca1"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-example"
+    Type = "tf-up-running"
+  }
+}
