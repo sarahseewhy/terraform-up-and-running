1ececfb09da73e00ea386957f3fb7e9eae3667c5
Sarah C Y <sarahseewhy@gmail.com>
2024-02-05 21:13:26 +0000

    feat(ec2): add instance & security group
    * Create an EC2 instance which runs a script -- this currently doesn't work as directed in the book but I haven't been able to figure out why yet and I want to move on
    * Create a security group for the EC2 instance

--- main.tf
+++ main.tf
@@ -3,11 +3,31 @@ provider "aws" {
 }
 
 resource "aws_instance" "this" {
-  ami           = "ami-09d6bbc1af02c2ca1"
-  instance_type = "t2.micro"
+  ami                    = "ami-0ff1c68c6e837b183"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.this.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello World" > index.html
+              nohup busybox httpd -f -p 8080 &
+  EOF
+
+  user_data_replace_on_change = true
 
   tags = {
     Name    = "terraform-example"
     Service = "tf-up-running"
+  }
+}
+
+resource "aws_security_group" "this" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
