53d0bec34dd77801d6b5628ca75ac56a104b3f5c
Sarah C Y <sarahseewhy@gmail.com>
2024-02-06 15:46:54 +0000

    feat: add alb security group

--- main.tf
+++ main.tf
@@ -63,6 +63,25 @@ resource "aws_lb_listener" "this" {
     }
   }
 }
+
+resource "aws_security_group" "alb" {
+  name = "terraform-alb-example"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
